7e0a9578ba36d61b46eb4cf5cd55bb00b61acc70
Nischita Nannapaneni <nischitanannapaneni@Nischitas-MacBook-Air.local>
2024-02-05 12:44:12 -0600

    did some work with the initial MVcomponent processing

---
+++ .github/MVcomponent/ai_grader.py
@@ -0,0 +1,19 @@
+#high level component that interacts with the server to process the data
+# - defines an interface for sending input file (pdf or image format)
+# - provides a mock student id and mock answers for each input file as a response
+# in the form of a list of answers for each question
+# - mock response is defined as a file
+
+import random
+
+class AIGrader:
+
+    def __init__():
+        
+    def load_response():
+        
+    def generate_response():
+        
+    def process_input():
+        
+    def get_studentID():
