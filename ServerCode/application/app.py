f6ca9940b4bb11733600a121ee76d20701f5b19a
mylesc <myles.correa@slu.edu>
2024-02-16 12:11:11 -0600

    added react code into app.py

--- ServerCode/application/app.py
+++ ServerCode/application/app.py
@@ -1,15 +1,23 @@
 from flask import Flask
 
+from flask_cors import CORS
+
 from application.rest import scantron
 
 def create_app(config_name):
     
-	app = Flask(__name__)
+    app = Flask(__name__)
 
-	config_module = f"application.config.{config_name.capitalize()}Config"
+    @app.route('/api/data', methods=['GET'])
+    def get_data():
+        return jsonify({'message': 'Hello from Flask!'})
+         
+    config_module = f"application.config.{config_name.capitalize()}Config"
 
-	app.config.from_object(config_module)
+    app.config.from_object(config_module)
 
-	app.register_blueprint(scantron.blueprint)
+    app.register_blueprint(scantron.blueprint)
+    
+    CORS(app)
 
-	return app
+    return app
