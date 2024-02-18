86be24f5800bd0c320d0d563784b0b00ea153208
RAJU KARMURI <78869779+rkarmuri@users.noreply.github.com>
2024-02-18 16:59:57 -0600

    Update app.py

--- ServerCode/application/app.py
+++ ServerCode/application/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-# from application.rest import scantron
+#from application import scantron
 app = Flask(__name__)
 
 @app.route('/api/data', methods=['GET'])
