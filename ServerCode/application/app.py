e0d88c9251f81a78338a3e4ce42461f59f2c530a
RAJU KARMURI <78869779+rkarmuri@users.noreply.github.com>
2024-02-18 16:48:09 -0600

    Fixed bugs in the app.py
    We are not using scantron.py for now. We are just trying to build a connection between the client and the server. I removed importing scatron.py here. Also, there are some bugs in setting up the connection. I fixed them here.

--- ServerCode/application/app.py
+++ ServerCode/application/app.py
@@ -1,32 +1,24 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from application.rest import scantron
+# from application.rest import scantron
+app = Flask(__name__)
 
-def create_app(config_name):
-    app = Flask(__name__)
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {"message": "Hello from Flask!"}
+    return jsonify(data)
 
-    @app.route('/api/data', methods=['GET'])
-    def get_data():
-        data = {"message": "Hello from Flask!"}
-        return jsonify(data)
+@app.route('/api/message', methods=['POST'])
+def receive_message():
 
-    @app.route('/api/message', methods=['POST'])
-    def receive_message():
+    message_data = request.json
+    message = message_data.get('message', '')
 
-        message_data = request.json
-        message = message_data.get('message', '')
+    print(f"Received message: {message}")
 
-        print(f"Received message: {message}")
+    return jsonify({"status": "success", "message": "Message received successfully!"})
 
-        return jsonify({"status": "success", "message": "Message received successfully!"})
-
-    config_module = f"application.config.{config_name.capitalize()}Config"
-    app.config.from_object(config_module)
-
-    app.register_blueprint(scantron.blueprint)
-
-    CORS(app)  
-    return app
+CORS(app)
 
 if __name__ == '__main__': 
     app.run(debug=True)
