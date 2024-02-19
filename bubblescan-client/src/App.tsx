f63a1b78076f3a3410a29da6c05a23c6f0d01e82
Ahmet Yildiz <ahmet.yildiz@slu.edu>
2024-02-19 10:20:42 -0600

    added header

--- bubblescan-client/src/App.tsx
+++ bubblescan-client/src/App.tsx
@@ -5,9 +5,10 @@ import FileUploadComponent from "./components/FileUploadComponent";
 function App() {
   return (
     <div>
-      <h3>What is your First and Last Name?</h3>
+      <h1>Welcome To Bubble Scan</h1>
+      <h2>What is your First and Last Name?</h2>
       <InputComponent />
-      <h3>You can upload your files below</h3>
+      <h2>You can upload your files below</h2>
       <FileUploadComponent />
     </div>
   );
