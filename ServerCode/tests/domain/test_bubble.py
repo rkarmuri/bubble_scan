5b4b9afca0305f789e34b9a7eb834b0ef9c6a1fe
Nischita Nannapaneni <nischitanannapaneni@Nischitas-MacBook-Air.local>
2024-02-26 13:04:02 -0600

    linted test_bubble; added docustrings

--- ServerCode/tests/domain/test_bubble.py
+++ ServerCode/tests/domain/test_bubble.py
@@ -2,9 +2,13 @@ import uuid
 from bubbleScan.domain.scantron import Scantron
 
 def test_scantron_model_init():
-    
+	"""Test scantron model initialization."""
 	code = uuid.uuid4()
-	scantron = Scantron(first, last, idNumber)
+	first = "John"
+	last = "Charlie"
+	idNumber = 34563
+
+	scantron = Scantron(code, first, last, idNumber)
 
 	assert scantron.code == code
 	assert scantron.first == "John"
@@ -12,6 +16,7 @@ def test_scantron_model_init():
 	assert scantron.idNumber == 34563
 
 def test_scantron_model_from_dict():
+	"""Test creating a Scantron model from a dictionary."""
 
 	code = uuid.uuid4()
 	init_dict = {"code": code, "first": "John", "last": "Charlie", "idNumber": 34563 }
@@ -24,7 +29,7 @@ def test_scantron_model_from_dict():
 	assert scantron.idNumber == 34563
 
 def test_scantron_model_to_dict():
-
+	"""Test converting a Scantron model to a dictionary."""
 	init_dict = {"code": uuid.uuid4(), "first": "John", "last": "Charlie", "idNumber": 34563 }
 
 	scantron = Scantron.from_dict(init_dict)
@@ -32,13 +37,10 @@ def test_scantron_model_to_dict():
 	assert scantron.to_dict() == init_dict
 
 def test_scantron_model_comparison():
+	"""Test comparing two Scantron models."""
 	init_dict = {"code": uuid.uuid4(), "first": "John", "last": "Charlie", "idNumber": 34563 }
 
 	scantron1 = Scantron.from_dict(init_dict)
 	scantron2 = Scantron.from_dict(init_dict)
 
 	assert scantron1 == scantron2
-
-
-
-
