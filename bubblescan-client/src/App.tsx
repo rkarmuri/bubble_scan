d58a309e82c795a366db93f05cab2fbc812178c2
Nischita Nannapaneni <nischitanannapaneni@Nischitas-MacBook-Air.local>
2024-03-04 15:08:06 -0600

    reabasing

--- bubblescan-client/src/App.tsx
+++ bubblescan-client/src/App.tsx
@@ -55,6 +55,8 @@ function App() {
         {response && <p>Response from sending message: {response}</p>}
       </div>
       <ListGroup />
+      <h3>What is your Name?</h3>
+      <InputComponent />
     </div>
   );
 }
